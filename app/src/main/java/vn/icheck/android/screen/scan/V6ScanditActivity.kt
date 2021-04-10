6282e6f23f040ebb238ee2cdbfc23ffd6268c7e9
sonvh <sonvh@icheck.vn>
2021-04-10 10:57:44 +0700

    sonvh update bugs

--- app/src/main/java/vn/icheck/android/screen/scan/V6ScanditActivity.kt
+++ app/src/main/java/vn/icheck/android/screen/scan/V6ScanditActivity.kt
@@ -41,6 +41,8 @@ import com.scandit.datacapture.core.data.FrameData
 import com.scandit.datacapture.core.source.*
 import com.scandit.datacapture.core.ui.DataCaptureView
 import com.scandit.datacapture.core.ui.DataCaptureViewListener
+import com.scandit.datacapture.core.ui.orientation.DeviceOrientation
+import com.scandit.datacapture.core.ui.orientation.DeviceOrientationMapper
 import kotlinx.android.synthetic.main.item_ads_product_grid.view.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -189,11 +191,16 @@ class V6ScanditActivity : BaseActivityMVVM(), BarcodeCaptureListener {
                             }
                             frameSource.removeListener(this)
                         } else {
+                            if (newState == FrameSourceState.STOPPING) {
+                                resetCamera()
+                            }
                             lastState = newState
                         }
                     }
+
                 })
                 resetHeight()
+
                 source?.switchToDesiredState(FrameSourceState.ON)
                 scanImage.set(true)
             }
@@ -222,6 +229,7 @@ class V6ScanditActivity : BaseActivityMVVM(), BarcodeCaptureListener {
         checkIsReview()
         checkIsScan()
         initViews()
+        pushUpHeight()
     }
 
     private fun checkIsScan() {
@@ -272,6 +280,13 @@ class V6ScanditActivity : BaseActivityMVVM(), BarcodeCaptureListener {
         _binding = IckScanCustomViewBinding.inflate(layoutInflater, dataCaptureView, false)
         dataCaptureView.addView(binding.root, getDeviceWidth(), getDeviceHeight())
         setContentView(dataCaptureView)
+        dataCaptureView.addListener(object : DataCaptureViewListener{
+            override fun onSizeChanged(width: Int, height: Int, screenRotation: Int) {
+                logDebug("orientation $screenRotation")
+                logDebug("width: $width")
+                logDebug("height $height")
+            }
+        })
     }
 
     private fun pushUpHeight() {
@@ -289,11 +304,13 @@ class V6ScanditActivity : BaseActivityMVVM(), BarcodeCaptureListener {
 
     private fun resetHeight() {
         dataCaptureView.post {
-            val lp = dataCaptureView.layoutParams
-            if (lp.height != getDeviceHeight()) {
-                lp.height = getDeviceHeight()
-                lp.width = getDeviceWidth()
-                dataCaptureView.layoutParams = lp
+            if (getUserCountry(this).contains("vn", false))  {
+                val lp = dataCaptureView.layoutParams
+                if (lp.height != getDeviceHeight()) {
+                    lp.height = getDeviceHeight()
+                    lp.width = getDeviceWidth()
+                    dataCaptureView.layoutParams = lp
+                }
             }
         }
     }
@@ -326,6 +343,7 @@ class V6ScanditActivity : BaseActivityMVVM(), BarcodeCaptureListener {
         })
     }
 
+
     private fun getUserCountry(context: Context): String {
         return try {
             val tm: TelephonyManager = context.getSystemService(TELEPHONY_SERVICE) as TelephonyManager
@@ -344,10 +362,10 @@ class V6ScanditActivity : BaseActivityMVVM(), BarcodeCaptureListener {
         }
 
         guideArr.add(binding?.imgScanTip)
+        guideArr.add(binding?.imgHdSdha)
         guideArr.add(binding?.imgNmspTip)
         guideArr.add(binding?.imgTorchTip)
         guideArr.add(binding?.imgXmdd)
-        guideArr.add(binding?.imgHdSdha)
         binding?.imgHelp?.setOnClickListener {
             viewModel.setGuide()
         }
--- icheckloyalty/src/main/java/vn/icheck/android/loyalty/screen/scan/V6ScanLoyaltyActivity.kt
+++ icheckloyalty/src/main/java/vn/icheck/android/loyalty/screen/scan/V6ScanLoyaltyActivity.kt
@@ -382,7 +382,7 @@ class V6ScanLoyaltyActivity : AppCompatActivity(), BarcodeCaptureListener {
         dataCaptureContext = DataCaptureContext.forLicenseKey(key)
         val settings = BarcodeCaptureSettings().apply {
             Symbology.values().forEach {
-                if (it != Symbology.MICRO_PDF417 && it != Symbology.PDF417) {
+                if (it != Symbology.MICRO_PDF417 && it != Symbology.PDF417  && it != Symbology.USPS_INTELLIGENT_MAIL) {
                     enableSymbology(it, true)
                     getSymbologySettings(it).isColorInvertedEnabled = true
                 }
@@ -429,11 +429,13 @@ class V6ScanLoyaltyActivity : AppCompatActivity(), BarcodeCaptureListener {
 
     private fun resetHeight() {
         dataCaptureView.post {
-            val lp = dataCaptureView.layoutParams
-            if (lp.height != getDeviceHeight()) {
-                lp.height = getDeviceHeight()
-                lp.width = getDeviceWidth()
-                dataCaptureView.layoutParams = lp
+            if (getUserCountry(this).contains("vn", false)) {
+                val lp = dataCaptureView.layoutParams
+                if (lp.height != getDeviceHeight()) {
+                    lp.height = getDeviceHeight()
+                    lp.width = getDeviceWidth()
+                    dataCaptureView.layoutParams = lp
+                }
             }
         }
 
