cb7aa11484fa259515339ed086449ffb6dee40e8
sonvh <sonvh@icheck.vn>
2021-04-20 09:04:40 +0700

    fix network

---
+++ ichecknetwork/src/main/java/vn/icheck/android/network/model/chat/ChatUser.kt
@@ -0,0 +1,10 @@
+package vn.icheck.android.network.model.chat
+
+class ChatUser{
+    var isOnline = false
+    var isVerify = false
+    var name = ""
+    var type = USER
+    var role = MEMBER
+    var image = ""
+}
