7dd1d38751e394d684fe1239d6ccfcd0c0663bab
kienpv <kienpv@icheck.vn>
2021-04-29 16:17:04 +0700

    fix camera verify

--- app/src/main/java/vn/icheck/android/screen/user/home/HomeActivity.kt
+++ app/src/main/java/vn/icheck/android/screen/user/home/HomeActivity.kt
@@ -26,6 +26,7 @@ import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.AppCompatCheckedTextView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
@@ -223,32 +224,32 @@ class HomeActivity : BaseActivity<HomePresenter>(), IHomeView, IScanHistoryView,
         listPage.add(ICFragment(null, HomePageFragment()))
         listPage.add(ICFragment(null, ListNewsFragment.newInstance(false)))
         listPage.add(ICFragment(null, ScanHistoryFragment()))
-        listPage.add(ICFragment(null, SocialChatFragment()))
-//        listPage.add(ICFragment(null, ChatSocialFragment().apply {
-//            setDataFromHome(object : ListConversationFragment.Companion.ICountMessageListener {
-//                override fun getCountMessage(count: Long) {
-//
-//                    val tvChatCount = findViewById<AppCompatTextView>(R.id.tvChatCount)
-//                    tvChatCount.post {
-//                        tvChatCount.visibility = if (count != 0L) {
-//                            View.VISIBLE
-//                        } else {
-//                            View.GONE
-//                        }
-//
-//                        tvChatCount.text = if (count > 9) {
-//                            "+9"
-//                        } else {
-//                            "$count"
-//                        }
-//                    }
-//                }
-//
-//                override fun onClickLeftMenu() {
-//                    openSlideMenu()
-//                }
-//            }, SessionManager.isUserLogged)
-//        }))
+//        listPage.add(ICFragment(null, SocialChatFragment()))
+        listPage.add(ICFragment(null, ChatSocialFragment().apply {
+            setDataFromHome(object : ListConversationFragment.Companion.ICountMessageListener {
+                override fun getCountMessage(count: Long) {
+
+                    val tvChatCount = findViewById<AppCompatTextView>(R.id.tvChatCount)
+                    tvChatCount.post {
+                        tvChatCount.visibility = if (count != 0L) {
+                            View.VISIBLE
+                        } else {
+                            View.GONE
+                        }
+
+                        tvChatCount.text = if (count > 9) {
+                            "+9"
+                        } else {
+                            "$count"
+                        }
+                    }
+                }
+
+                override fun onClickLeftMenu() {
+                    openSlideMenu()
+                }
+            }, SessionManager.isUserLogged)
+        }))
 
         viewPager.offscreenPageLimit = 5
         viewPager.setPagingEnabled(false)
