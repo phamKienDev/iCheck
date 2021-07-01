599e694bc59c3b8acf321b8e11a7cd9fa522038f
KienNT <kiennt@rabiloo.com.vn>
2021-07-01 14:15:52 +0700

    Lỗi hiển thị quảng cáo

--- ichecklibs/src/main/java/vn/icheck/android/ichecklibs/MiddleMultilineTextView.kt
+++ ichecklibs/src/main/java/vn/icheck/android/ichecklibs/MiddleMultilineTextView.kt
@@ -1,63 +1,49 @@
-package vn.icheck.android.ichecklibs;
+package vn.icheck.android.ichecklibs
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.util.AttributeSet;
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
 
-public class MiddleMultilineTextView extends androidx.appcompat.widget.AppCompatTextView {
+open class MiddleMultilineTextView : AppCompatTextView {
+    private val SYMBOL = " ... "
+    private val SYMBOL_LENGTH = SYMBOL.length
 
-    private String SYMBOL = " ... ";
-    private final int SYMBOL_LENGTH = SYMBOL.length();
+    constructor(context: Context?) : super(context!!) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr) {}
 
-    public MiddleMultilineTextView(Context context) {
-        super(context);
-    }
-    public MiddleMultilineTextView(Context context, AttributeSet attrs) { super(context, attrs); }
-    public MiddleMultilineTextView(Context context, AttributeSet attrs,int defStyleAttr) { super(context, attrs,defStyleAttr); }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        if (getMaxLines() > 1) {
-            int originalLength = getText().length();
-            int visibleLength = getVisibleLength();
-
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (maxLines > 1) {
+            val originalLength = text.length
+            val visibleLength = visibleLength
             if (originalLength > visibleLength) {
-                setText(smartTrim(getText().toString(), visibleLength - SYMBOL_LENGTH));
+                text = smartTrim(text.toString(), visibleLength - SYMBOL_LENGTH)
             }
         }
     }
 
-
-    private String smartTrim(String string, int maxLength) {
-        if (string == null)
-            return null;
-        if (maxLength < 1)
-            return string;
-        if (string.length() <= maxLength)
-            return string;
-        if (maxLength == 1)
-            return string.substring(0, 1) + "...";
-
-        int midpoint = (int) Math.ceil(string.length() / 2);
-        int toremove = string.length() - maxLength;
-        int lstrip = (int) Math.ceil(toremove / 2);
-        int rstrip = toremove - lstrip;
-
-        int subTo = midpoint - lstrip - 4;
-        if (subTo < 0){
-            subTo = 0;
+    private fun smartTrim(string: String?, maxLength: Int): String? {
+        if (string == null) return null
+        if (maxLength < 1) return string
+        if (string.length <= maxLength) return string
+        if (maxLength == 1) return string.substring(0, 1) + "..."
+        val midpoint = Math.ceil((string.length / 2).toDouble()).toInt()
+        val toremove = string.length - maxLength
+        val lstrip = Math.ceil((toremove / 2).toDouble()).toInt()
+        val rstrip = toremove - lstrip
+        var subTo = midpoint - lstrip - 4
+        if (subTo < 0) {
+            subTo = 0
         }
-
-        String result = string.substring(0, subTo) + SYMBOL + string.substring(midpoint + rstrip);
-
-        return result;
+        return string.substring(0, subTo) + SYMBOL + string.substring(midpoint + rstrip)
     }
 
-    private int getVisibleLength() {
-        int start = getLayout().getLineStart(0);
-        int end = getLayout().getLineEnd(getMaxLines() - 1);
-        return getText().toString().substring(start, end).length();
-    }
+    private val visibleLength: Int
+        private get() {
+            val start = layout.getLineStart(0)
+            val end = layout.getLineEnd(maxLines - 1)
+            return text.toString().substring(start, end).length
+        }
 }
